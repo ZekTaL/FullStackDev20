6854d8639e0b38e8aa68d39d09cdb6f580537f62
ZekTaL <francykiller@hotmail.it>
2020-09-02 22:17:34 +1000

    update to 3.8

--- part3/Phonebook Backend [3.8]/index.js
+++ part3/Phonebook Backend [3.8]/index.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+
+morgan.token('data', function getData (req, res) {
+    if (req.method === 'POST')
+        return JSON.stringify(req.body)
+    else
+        return null
+
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
    
+
+// HARDCODED PERSONS
 let persons = [
     {
     "name": "Arto Hellas",
