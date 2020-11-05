619ad5c72977586db03b1aa40257b04d9ebb59a9
ZekTaL <francykiller@hotmail.it>
2020-11-05 11:04:23 +1100

    note 6a

--- notes/src/index.js
+++ notes/src/index.js
@@ -2,5 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import noteReducer from './reducers/noteReducer'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(noteReducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,   
+    document.getElementById('root')
+)
