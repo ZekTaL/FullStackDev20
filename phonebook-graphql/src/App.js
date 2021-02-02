8738f9ef9b050d27e1786d3b2dc88c180eec76b8
ZekTaL <francykiller@hotmail.it>
2021-02-02 23:30:06 +1100

    update part8b

---
+++ phonebook-graphql/src/App.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import {useQuery } from '@apollo/client'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import {ALL_PERSONS} from './queries'
+import PhoneForm from './components/PhoneForm'
+
+const Notify = ({errorMessage}) => {  
+  if ( !errorMessage ) {    
+    return null  
+  } 
+  
+  return (    
+    <div style={{color: 'red'}}>
+      {errorMessage}
+    </div>  
+  )
+}
+
+const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
+
+  const notify = (message) => {    
+    setErrorMessage(message)    
+    setTimeout(() => {      
+      setErrorMessage(null)    
+    }, 10000) 
+  }
+
+  return (
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify}/>
+      <Persons persons = {result.data.allPersons} />
+    </div>
+  )
+}
+
+export default App
