210959c62b3bf6af97d08e7fc37c1c486ce9edbb
ZekTaL <francykiller@hotmail.it>
2021-02-09 00:45:34 +1100

    update part 8d

--- phonebook-graphql/src/App.js
+++ phonebook-graphql/src/App.js
@@ -1,9 +1,10 @@
-import React, {useState} from 'react'
-import {useQuery } from '@apollo/client'
+import React, {useState, useEffect} from 'react'
+import {useQuery, useApolloClient } from '@apollo/client'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import {ALL_PERSONS} from './queries'
 import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
 
 const Notify = ({errorMessage}) => {  
   if ( !errorMessage ) {    
@@ -19,11 +20,26 @@ const Notify = ({errorMessage}) => {
 
 const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
+  const [token, setToken] = useState(null)
 
   const result = useQuery(ALL_PERSONS)
+  const client = useApolloClient()
+
+  useEffect(() => {
+    const token = localStorage.getItem('phonenumbers-user-token')
+    if ( token ) {
+      setToken(token)
+    }
+  }, [])
 
   if (result.loading)  {
     return <div>loading...</div>
+  }
+
+  const logout = () => {    
+    setToken(null)    
+    localStorage.clear()    
+    client.resetStore()  
   }
 
   const notify = (message) => {    
@@ -33,12 +49,26 @@ const App = () => {
     }, 10000) 
   }
 
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </div>
+    )
+  }
+
   return (
     <div>
+      <button onClick={logout} >logout</button>
       <Notify errorMessage={errorMessage} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify}/>
-      <Persons persons = {result.data.allPersons} />
+      {
+        !result.data ? <h2>No Persons added!</h2>
+                     : <Persons persons = {result.data.allPersons} />
+      }
     </div>
   )
 }
